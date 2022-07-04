9aea313b8358a850d08c05f8b0f17c3c40256ab5
MicaelaBallesta <micaelaballesta@gmail.com>
2022-07-03 22:32:56 -0300

    arreglé algo de la ruta en app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.listen(3000, ()=>{
 });
 
 app.get('/', (req,res)=>{
-    res.sendFile(__dirname + '/views/index.html');
+    res.sendFile(__dirname + './views/index.html');
 });
 
 app.get("/login", (req,res) => {
