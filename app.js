e5bebcbda8769c646e5b67c7ad1f919e522d167d
MicaelaBallesta <micaelaballesta@gmail.com>
2022-07-06 17:57:05 -0300

    arregle app.js

--- app.js
+++ app.js
@@ -13,18 +13,18 @@ app.get('/', (req,res)=>{
 });
 
 app.get("/login", (req,res) => {
-let login = path.join(__dirname, "/views/login.html") 
+let login = path.join(__dirname, '/views/login.html') 
 res.sendFile(login) }); 
 
-app.get("/productCart.html", (req,res) => {
-let productCart = path.join(__dirname, "/views/productCart.html") 
+app.get("/productCart", (req,res) => {
+let productCart = path.join(__dirname, '/views/productCart.html') 
 res.sendFile(productCart) }); 
 
-app.get("/productDetail.html", (req,res) => {
-let productDetail = path.join(__dirname, "/views/productDetail.html") 
+app.get("/productDetail", (req,res) => {
+let productDetail = path.join(__dirname, '/views/productDetail.html') 
 res.sendFile(productDetail) }); 
 
-app.get("/register.html", (req,res) => {
-let register = path.join(__dirname, "/views/register.html") 
+app.get("/register", (req,res) => {
+let register = path.join(__dirname, '/views/register.html') 
 res.sendFile(register) }); 
     
