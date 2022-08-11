5a0c4110167a0e8a193b6c1411968f220fc95d5e
MicaelaBallesta <micaelaballesta@gmail.com>
2022-08-10 23:06:09 -0300

    cambios

--- src/routes/main.js
+++ src/routes/main.js
@@ -1,12 +1,18 @@
 var express = require ("express");
+const indexController = require("../controllers/indexController");
 var router = express.Router();
 var controller = require ("../controllers/indexController");
 
 
 /* get home page */
-router.get ("index", indexController.index);
+router.get ("/", indexController.index);
+
+router.get ("/catalogo", indexController.catalogo);
+
+router.get("/tienda", indexController.tienda);
 
 router.get("/")
+
 
 module.exports = router;
 
