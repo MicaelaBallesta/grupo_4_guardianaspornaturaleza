a002413ff2e0a08e03302941a508b0416836938c
leonardos_ferreyra@yahoo.com.ar <leonardos_ferreyra@yahoo.com.ar>
2022-08-19 01:16:21 -0300

    otra vez

---
+++ grupo_4_guardianaspornaturaleza-main/app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const path = require('path')
+app.use(express.static('public'));
+
+
+app.listen(3000, ()=>{
+    console.log('Servidor funcionando');
+});
+
+app.get('/', (req,res)=>{
+    res.sendFile(__dirname + '/views/index.html');
+});
+
+app.get("/login", (req,res) => {
+let login = path.join(__dirname, '/views/login.html') 
+res.sendFile(login) }); 
+
+app.get("/productCart", (req,res) => {
+let productCart = path.join(__dirname, '/views/productCart.html') 
+res.sendFile(productCart) }); 
+
+app.get("/productDetail", (req,res) => {
+let productDetail = path.join(__dirname, '/views/productDetail.html') 
+res.sendFile(productDetail) }); 
+
+app.get("/register", (req,res) => {
+let register = path.join(__dirname, '/views/register.html') 
+res.sendFile(register) }); 
+    
