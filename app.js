7eeb547be890603ad673f83160a46b47daf9b935
leonardos_ferreyra@yahoo.com.ar <leonardos_ferreyra@yahoo.com.ar>
2022-09-02 00:01:45 -0300

    modifico app

--- app.js
+++ app.js
@@ -36,4 +36,42 @@ app.use(methodOverride("_method"));
 app.use(express.urlencoded({ extended:false}));
 app.use(express.json());
 
-    
+//const express = require("express"); // MODULO EXPRESS
+//const app = express(); // MODULO EXPRESS EN FUNCIONALIDAD EN APP.JS
+//const path = require("path"); //  MODULO PATH
+//app.use(express.static("./Public")); // CSS
+//var methodOverride = require('method-override');
+
+//console.log(__dirname);
+
+//app.listen(3030, () => {
+ // console.log("Servidor funcionando");
+//});
+
+//app.use(methodOverride('_method'));
+
+
+//app.set('view engine', 'ejs');
+//app.set('views', path.join(__dirname, '/src/views')); // Define la ubicación de la carpeta de las Vistas
+
+// MVC SYSTEM
+  
+// Main Routes
+//const rutasMain = require("./src/routes/main");
+//app.use('/', rutasMain);
+
+// Users Routes
+//const rutasUsers = require("./src/routes/users");
+//app.use('/users', rutasUsers);
+//Registro Routes
+//const rutasRegistro = require("./src/routes/registro");
+//app.use('/registro', rutasRegistro);
+//Products Routes
+//const rutasProductos = require("./src/routes/products");
+//app.use('/products', rutasProductos);
+//Campos Routes
+//const rutasCampos = require("./src/routes/campos");
+//app.use('/campos', rutasCampos);
+//Lecciones Routes
+//const rutasLecciones = require("./src/routes/lecciones");
+//app.use('/lecciones', rutasLecciones);
