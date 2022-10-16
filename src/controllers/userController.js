1e9f62c8c7eea49a1f24c237fef707264c00f3f3
MicaelaBallesta <micaelaballesta@gmail.com>
2022-10-15 21:42:45 -0300

    ruta register

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,14 +1,10 @@
 const fs = require('fs');
 const path = require('path');
 
-const productsFilePath = path.join(__dirname, '../data/userDataBase.json');
-const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
-
-
 
 const controller = {
 	register: (req, res) => {
-		res.render('register');
+		res.render('users/register');
 	}
 };
 
